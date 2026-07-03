0d93dbff7236b802060e48c99a84be0e458654a7
rainandakhumairoh <rnkhumairoh@gmail.com>
2026-07-03 20:19:55 +0700

    hapus app.listen di index.js

--- ourfit-backend/index.js
+++ ourfit-backend/index.js
@@ -33,4 +33,5 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error(err));
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+export default app;
